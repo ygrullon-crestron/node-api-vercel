dd44a51c4e00d88f24150cfec731ae39019fd423
ygrullon-crestron <111453772+ygrullon-crestron@users.noreply.github.com>
2023-04-04 01:39:18 -0400

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.send('Hey this is my API running 🥳 hahaha')
 })
 
 app.get('/about', (req, res) => {
