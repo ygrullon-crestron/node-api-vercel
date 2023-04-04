673295e319fb1564f78249ebc90ab2c13079cf3b
ygrullon-crestron <111453772+ygrullon-crestron@users.noreply.github.com>
2023-04-04 02:31:19 -0400

    moved called

--- index.js
+++ index.js
@@ -16,5 +16,29 @@ app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
+
+const mysql = require("mysql2/promise");
+
+const insertIntoDB = async () => {
+    const connection = await mysql.createConnection({
+        host: "sql9.freemysqlhosting.net",
+        user: "sql9610911",
+        password: "NaSUMz2EYi",
+        database: "sql9610911",
+        port: 3306,
+    })
+
+    try {
+        await connection.query(
+            "INSERT INTO teamMembers (id,FirstName,LastName,Email,Location,Role,Application,ProfilePicture) VALUES('0','yesmar','grullon','grullon.y@live.com','new york','admin','all','/some/url')"
+        )
+        console.log("inserted!")
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+insertIntoDB();
+
 // Export the Express API
 module.exports = app
